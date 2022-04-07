f09de15f3a6a97ad9b438534f34795a37e3d5ed1
Chany <inshakr2@naver.com>
2022-04-08 08:38:24 +0900

    프로젝트 생성

---
+++ src/main/java/com/toy/chanygram/ChanygramApplication.java
@@ -0,0 +1,13 @@
+package com.toy.chanygram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChanygramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChanygramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toy/chanygram/ChanygramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.chanygram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChanygramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
