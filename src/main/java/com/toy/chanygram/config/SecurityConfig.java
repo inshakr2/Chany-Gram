983cafebbe57510e48b2da6b10678d15506ce8d9
Chany <inshakr2@naver.com>
2022-04-20 18:57:22 +0900

    Security 적용 URL 추가

--- src/main/java/com/toy/chanygram/config/SecurityConfig.java
+++ src/main/java/com/toy/chanygram/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests()
-                .antMatchers("/", "/user/**", "/images/**").authenticated()
+                .antMatchers("/", "/user/**", "/images/**", "/subscribe/**", "/api/**").authenticated()
                 .anyRequest().permitAll()
                 .and()
                 .formLogin().loginPage("/auth/signin")
